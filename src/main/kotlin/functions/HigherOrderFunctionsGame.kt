13f4379e5dfd01d0e34dceef184fc2807f517ffe
Gaurav Singh <gaurav.singh@go-jek.com>
2019-03-19 20:46:42 +0530

    Working on a game

--- src/main/kotlin/functions/HigherOrderFunctionsGame.kt
+++ src/main/kotlin/functions/HigherOrderFunctionsGame.kt
@@ -48,12 +48,12 @@ class Game {
         false
     }
 
-    fun move(where : () -> Boolean) : Unit {
-        where()
+    fun move(where: () -> Boolean) {
+        where.invoke()
     }
 
-    fun makeMove(like : String?) {
-        when (like) {
+    fun makeMove(command: String?) {
+        when (command) {
             "n" -> move(north)
             "s" -> move(south)
             "e" -> move(east)
@@ -66,24 +66,75 @@ class Game {
 fun main(args: Array<String>) {
     val game = Game()
 
-    while(true) {
+    while (true) {
         println("Enter a direction: n/s/e/w")
         val userChoice = readLine()
         game.makeMove(userChoice)
     }
 }
 
-class Location(var width : Int = 4, var height : Int = 4) {
-    val map = Array(width) { arrayOfNulls<String>(height)}
-    val initialLocation = Pair(1, 1)
+class Location(var width: Int = 4, var height: Int = 4) {
+    val map = Array(width) { arrayOfNulls<String>(height) }
+    var initialLocation = Pair(0, 0)
 
     init {
-       map[0] = arrayOf("karnataka", "tamil nadu", "tamil nadu", "kerela")
-       map[1] = arrayOf("asgard", "krypton", "Zune", "heimdall")
+        map[0] = arrayOf("karnataka", "tamil nadu", "tamil nadu", "kerela")
+        map[1] = arrayOf("asgard", "krypton", "Zune", "heimdall")
+        map[2] = arrayOf("vienna", "paris", "london", "zurich")
+        map[3] = arrayOf("lucknow", "meerut", "prayagraj", "saharanpur")
     }
 
     // Complete the above.
-    fun updateLocation(move : Int) {
+//    fun updateLocation(move: Char): Pair {
+//        when (move) {
+//            'n' -> return Pair(initialLocation.first , (initialLocation.second + 1).rem(height))
+//            's' -> return Pair(initialLocation.first , (initialLocation.second - 1).rem(height))
+//            'e' -> return Pair(initialLocation.first , (initialLocation.second + 1).rem(width))
+//            'w' ->
+//            else ->
+//        }
 
-    }
+//    }
+    /*
+    Practice Time
+
+In this practice, you will finish your simple game using higher-order functions, that is, a function that takes functions as an argument.
+
+    In the game class, create a function move() that takes an argument called where, which is a lambda with no arguments that returns Unit.
+
+    Hint: Declaring a function that takes a lambda as its argument:
+
+    fun move(where: () -> Boolean )
+
+    Inside move(), invoke the passed-in lambda.
+    In the Game class, create a function makeMove() that takes a nullable String argument and returns nothing.
+
+    Inside makeMove, test whether the String is any of the 4 directions and invoke move() with the corresponding lambda. Otherwise, invoke move() with end.
+
+    Hint: You can call the function like this:
+
+    move(north)
+
+    In main() add a while loop that is always true.
+    Inside the loop, print instructions to the player:
+
+    print("Enter a direction: n/s/e/w:")
+
+    Call makeMove() with the contents of the input from the user via readLine()
+    Remove the code for testing the first version of your game.
+    Run your program.
+
+Challenge:
+
+Create a simple “map” for your game, and when the user moves, show a description of their location. Consider the following:
+
+    Use a Location class that takes a default width and height to track location. 4x4 is pretty manageable.
+    You can create a matrix like this:
+
+    val map = Array(width) { arrayOfNulls<String>(height) }
+
+    Use an init block to initialize your map with descriptions for each location.
+    When you move() also updateLocation(). There is some math involved to prevent null-pointer exceptions and keep the user from walking off the map. rem() and absoluteValue come handy.
+    When you are done, zip up the code and send it to a friend to try your first Kotlin game.
+     */
 }
