87846a230ef3dbf1ec505e820000caf445c8a7c7
Gaurav Singh <gaurav.singh@go-jek.com>
2019-05-11 21:35:10 +0530

    Added example of Inheritance in kotlin and a basic playground example for using TestNG in kotlin

--- src/main/kotlin/experiments/TestNGExperiments.kt
+++ src/main/kotlin/experiments/TestNGExperiments.kt
@@ -1,2 +1,57 @@
 package experiments
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import org.testng.annotations.DataProvider
+import org.testng.annotations.Test
+
+val someJson = """
+    {
+  "cases": [
+    {
+      "name": "foo",
+      "desc": "foo desc "
+    },
+    {
+      "name": "bar",
+      "desc": "bar desc"
+    }
+  ]
+}
+
+""".trimIndent()
+
+data class Cases(
+        val cases: List<Case>
+)
+
+data class Case(
+        val name: String,
+        val desc: String
+)
+
+fun getData(): Cases {
+    val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    val adapter = moshi.adapter(Cases::class.java)
+    return adapter.fromJson(someJson)!!
+}
+
+class SomeTests {
+    @DataProvider(name = "theDP")
+    fun dataProvider(): MutableIterator<Array<Any>> {
+        val cases = getData()
+        val dp: ArrayList<Array<Any>> = ArrayList()
+
+        for (case in cases.cases) {
+            dp.add(arrayOf(case.name, case.desc))
+        }
+
+        return dp.iterator()
+    }
+
+    @Test(dataProvider = "theDP")
+    fun someTest(name: String, description: String) {
+        println("$name $description")
+    }
+
+}
--- src/main/kotlin/koans/_04_classes/Inheritance.kt
+++ src/main/kotlin/koans/_04_classes/Inheritance.kt
@@ -3,5 +3,62 @@ package koans._04_classes
 /**
  * All classes in kotlin inherit from Any
  * Any is not equivalent to java.lang.Object and only has equals(), hashCode() and toString() methods
+ * Refer to Java interop section to understand more: https://kotlinlang.org/docs/reference/java-interop.html#object-methods
  */
-class Computer
+
+// All classes are final by default in kotlin and hence needs to be declared with open keyword
+// to enable to be subclassed
+// KOTLIN LIKES TO BE VERY EXPLICIT in terms of inheritance
+
+open class Computer {
+    // private to Computer, child classes cannot override this
+    fun whichType() {
+        println("Specific type")
+    }
+
+    // Can be overridden by child class
+    open fun whichScreen() {
+        println("Parent: Base classes screen type")
+    }
+
+    open fun processorType() {
+        println("Parent: Generic processor")
+    }
+
+}
+
+// To inherit use colon after class header followed by super class
+// If super class has a primary constructor then child class needs to initialize it.
+// https://kotlinlang.org/docs/reference/classes.html?_ga=2.208758544.308914685.1557588520-2114576280.1553576364#inheritance
+// Overriding methods: https://kotlinlang.org/docs/reference/classes.html?_ga=2.208758544.308914685.1557588520-2114576280.1553576364#overriding-methods
+
+open class Macs : Computer() {
+
+    // In child class: override keyword is mandatory
+    override fun whichScreen() {
+        println("Child: Macs's retina screen...")
+        super.whichScreen()
+    }
+
+    // Make a class func as final to prevent child classes to override
+    // By nature override of processorType() here would be open for overriding in child class.
+    final override fun processorType() {
+        println("Child: Lets make final so that i cannot be overridden")
+    }
+
+}
+
+class IMac : Macs() {
+    override fun whichScreen() {
+        println("2nd Level screen: IMacs large screen")
+    }
+
+    // Cannot override processorType() since it is final in Macs class
+
+}
+
+fun main(args: Array<String>) {
+    val mac = Macs()
+    mac.whichScreen()
+
+}
