a6c85b90f1442a032e371d473bb4a425933b9688
Gaurav Singh <gaurav.singh@go-jek.com>
2019-06-02 21:17:04 +0530

    Kotlin vs Java: 1. Example of bean vs kotlin class 2. Example using data classes to model objects and how easy it is to use.

---
+++ src/main/java/_01_bean/Person.java
@@ -0,0 +1,19 @@
+package _01_bean;
+
+public class Person {
+    private final String name;
+    private final Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+}
---
+++ src/main/java/_01_bean/PersonKT.kt
@@ -0,0 +1,5 @@
+package _01_bean
+
+class PersonKT(
+        val name: String,
+        val age: Int)
