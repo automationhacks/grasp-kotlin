0740e712b5da54c92df95e1e0773a39a782cad5d
Gaurav Singh <gaurav.singh@go-jek.com>
2019-06-09 22:34:23 +0530

    Added main func to demonstrate how properties can be directly accessed via dot and don't require get/set syntax.

--- src/main/java/_01_bean/PersonKT.kt
+++ src/main/java/_01_bean/PersonKT.kt
@@ -3,3 +3,10 @@ package _01_bean
 class PersonKT(
         val name: String,
         val age: Int)
+
+fun main(args: Array<String>) {
+    val person = PersonKT("Jack", 30)
+
+    println(person.name)
+    println(person.age)
+}
