4f2c73ddcaca0316780d2f70c18898192ff5f223
Gaurav Singh <gaurav.singh@go-jek.com>
2019-06-15 11:13:07 +0530

    Added example of using Extension properties

--- src/main/java/_03_functions/Functions.kt
+++ src/main/java/_03_functions/Functions.kt
@@ -5,8 +5,18 @@ fun String.getFirstWord(separator: String = " "): String {
     return this.split(separator)[0]
 }
 
+// Example of extension properties
+// In this case on top of a String class
+val String.lastWord: String
+    get() {
+        return this.split("")[1]
+    }
+
 fun main() {
     val first = "Jane Doe".getFirstWord()
+    val last = "John Doe".lastWord
+
     println("First word: $first")
+    println("Last word (Using extension properties) $last")
 }
 
