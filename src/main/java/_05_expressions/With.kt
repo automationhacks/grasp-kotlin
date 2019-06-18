62fd55f79c31e0da36030ee0d9ca2a3fea2f8884
Gaurav Singh <gaurav.singh@go-jek.com>
2019-06-18 19:44:19 +0530

    Added initial example of expressions

---
+++ src/main/java/_05_expressions/With.kt
@@ -0,0 +1,13 @@
+package _05_expressions
+
+class Robot(val name: String, val function: String, val weight: Int)
+
+fun main() {
+    var chappie = Robot("Chappie", "Fight", 25)
+
+    val name = chappie.name
+    val function = chappie.function
+    val weight = chappie.weight
+
+    println("Robot is $name who does $function with weight $weight")
+}
