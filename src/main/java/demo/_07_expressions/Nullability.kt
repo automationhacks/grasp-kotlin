95d4fd0708c31f278fb60007b410d61ff7d7bef9
Gaurav Singh <gaurav.singh@go-jek.com>
2019-08-06 21:42:15 +0530

    Demo: Understanding nullability

---
+++ src/main/java/demo/_07_expressions/Nullability.kt
@@ -0,0 +1,21 @@
+package demo._07_expressions
+
+fun readMeByChar(text: String?): String? {
+    println(text?.length)
+
+    if (text != null) {
+        text.forEach { it -> println(it) }
+    }
+
+    // Typical ways of handing null values
+    if (text == null) return null
+    if (text == null) throw RuntimeException("Unexpected null")
+    if (text == null) error("Unexpected null")
+
+    return ""
+}
+
+fun main() {
+    readMeByChar("Welcome to wonderful world of kotlin!")
+    readMeByChar(null)
+}
