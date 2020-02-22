62653560d6b7973a06a06531d90f8d5d60c22aee
Gaurav Singh <gaurav.singh55@gmail.com>
2020-02-22 08:49:03 +0530

    Modified Booking to vehicle type Added data provider to the test methods.

--- src/test/kotlin/testFrameworks/testNG/setupTearDown/SetupTeardownWithDataProvider.kt
+++ src/test/kotlin/testFrameworks/testNG/setupTearDown/SetupTeardownWithDataProvider.kt
@@ -1,12 +1,13 @@
 package testFrameworks.testNG.setupTearDown
 
 import org.testng.Assert
+import org.testng.annotations.DataProvider
 import org.testng.annotations.Test
 import java.util.concurrent.ThreadLocalRandom
 
-enum class BookingType(val type: String) {
-    RAPID("rapid"),
-    SLOW("slow")
+enum class VehicleType() {
+    CAR,
+    CAR_XL
 }
 
 class BookingNotFound(message: String) : Exception(message)
@@ -14,15 +15,15 @@ class BookingNotFound(message: String) : Exception(message)
 
 class Booking {
     companion object {
-        private val bookingDB = hashMapOf<BookingType, ArrayList<Int>>()
+        private val bookingDB = hashMapOf<VehicleType, ArrayList<Int>>()
     }
 
     init {
-        bookingDB[BookingType.RAPID] = arrayListOf()
-        bookingDB[BookingType.SLOW] = arrayListOf()
+        bookingDB[VehicleType.CAR] = arrayListOf()
+        bookingDB[VehicleType.CAR_XL] = arrayListOf()
     }
 
-    fun makeBooking(type: BookingType): Int {
+    fun makeBooking(type: VehicleType): Int {
         val orderId = getRandomInt()
         addBookingToDb(type, orderId)
 
@@ -30,24 +31,24 @@ class Booking {
     }
 
     private fun getRandomInt() = ThreadLocalRandom.current().nextInt(1, 1000)
-    private fun addBookingToDb(type: BookingType, orderId: Int) {
+    private fun addBookingToDb(type: VehicleType, orderId: Int) {
         bookingDB[type]?.add(orderId)
     }
 
-    fun cancelBooking(type: BookingType, orderId: Int) {
+    fun cancelBooking(type: VehicleType, orderId: Int) {
         val doesOrderExist = doesBookingExists(type, orderId)
         cancelBookingIfFound(doesOrderExist, orderId, type)
     }
 
 
-    fun doesBookingExists(type: BookingType, orderId: Int): Boolean? {
+    fun doesBookingExists(type: VehicleType, orderId: Int): Boolean? {
         return bookingDB[type]?.contains(orderId)
     }
 
     private fun cancelBookingIfFound(
         doesOrderExist: Boolean?,
         orderId: Int,
-        type: BookingType
+        type: VehicleType
     ) {
         if (doesOrderExist == null) {
             throw BookingNotFound("Order $orderId not found in db. Cannot cancel.")
@@ -59,29 +60,37 @@ class Booking {
 }
 
 class SetupTeardownWithDataProvider {
-    @Test
-    fun testBookingCreation() {
-        val bookingType = BookingType.SLOW
+    @DataProvider
+    fun getVehicleTypes(): MutableIterator<Array<Any>> {
+        val vehicles = arrayListOf<Array<Any>>()
 
+        vehicles.add(arrayOf(VehicleType.CAR))
+        vehicles.add(arrayOf(VehicleType.CAR_XL))
+
+        return vehicles.iterator()
+    }
+
+    @Test(dataProvider = "getVehicleTypes")
+    fun testBookingCreation(vehicleType: VehicleType) {
         val booking = Booking()
-        val orderId = booking.makeBooking(bookingType)
+        val orderId = booking.makeBooking(vehicleType)
 
-        val isBookingCreated = booking.doesBookingExists(bookingType, orderId)
+        val isBookingCreated = booking.doesBookingExists(vehicleType, orderId)
         Assert.assertNotNull(isBookingCreated)
     }
 
     @Test
     fun testBookingCancellation() {
-        val bookingType = BookingType.SLOW
+        val vehicleType = VehicleType.CAR_XL
 
         val booking = Booking()
-        val orderId = booking.makeBooking(bookingType)
+        val orderId = booking.makeBooking(vehicleType)
 
-        val isBookingCreated = booking.doesBookingExists(bookingType, orderId)
+        val isBookingCreated = booking.doesBookingExists(vehicleType, orderId)
         Assert.assertNotNull(isBookingCreated)
 
-        booking.cancelBooking(bookingType, orderId)
-        val isBookingFound = booking.doesBookingExists(bookingType, orderId)
+        booking.cancelBooking(vehicleType, orderId)
+        val isBookingFound = booking.doesBookingExists(vehicleType, orderId)
 
         Assert.assertNotNull(isBookingCreated)
         Assert.assertFalse(isBookingFound!!)
